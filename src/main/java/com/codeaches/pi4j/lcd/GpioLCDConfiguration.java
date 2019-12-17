e41fafe02251b00f40b0c583a59861de8c7b0872
codeaches <info001@codeaches.com>
2019-12-16 19:51:39 -0700

    no message

--- src/main/java/com/codeaches/pi4j/lcd/GpioLCDConfiguration.java
+++ src/main/java/com/codeaches/pi4j/lcd/GpioLCDConfiguration.java
@@ -1,5 +1,8 @@
 package com.codeaches.pi4j.lcd;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.annotation.PreDestroy;
 
 import org.slf4j.Logger;
@@ -7,53 +10,149 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.pi4j.component.lcd.LCDTextAlignment;
 import com.pi4j.component.lcd.impl.GpioLcdDisplay;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
-import com.pi4j.wiringpi.Gpio;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 @Configuration
 public class GpioLCDConfiguration {
 
   Logger log = LoggerFactory.getLogger(GpioLCDConfiguration.class);
 
+  public final static int LCD_ROWS = 2;
+
+  public final static int LCD_COLUMNS = 16;
   public final static int LCD_ROW_1 = 0;
   public final static int LCD_ROW_2 = 1;
+
+  public final static int LCD_BITS = 4;
 
   @Bean("lcd")
   public GpioLcdDisplay lcd() throws Exception {
 
-    // Setup wiringPi
-    if (Gpio.wiringPiSetupGpio() == -1) {
-      throw new RuntimeException("Gpio.wiringPiSetupGpio failed...");
-    }
+    System.out.println("<--Pi4J--> GPIO 4 bit LCD example program");
 
-    GpioLcdDisplay out = new GpioLcdDisplay(2, // number of rows supported by LCD
-        16, // number of columns supported by LCD
-        RaspiPin.GPIO_09, // LCD RS pin
-        RaspiPin.GPIO_08, // LCD strobe pin
-        RaspiPin.GPIO_07, // LCD data bit D4
-        RaspiPin.GPIO_15, // LCD data bit D5
-        RaspiPin.GPIO_16, // LCD data bit D6
-        RaspiPin.GPIO_01); // LCD data bit D7
+    // create gpio controller
+    final GpioController gpio = GpioFactory.getInstance();
 
-    log.info("GpioLcdDisplay initialized");
+    // initialize LCD
+    final GpioLcdDisplay lcd = new GpioLcdDisplay(//
+        LCD_ROWS, // number of row supported by LCD
+        LCD_COLUMNS, // number of columns supported by LCD
+        RaspiPin.GPIO_11, // LCD RS pin
+        RaspiPin.GPIO_10, // LCD strobe pin/EN pin
+        RaspiPin.GPIO_00, // LCD data bit 1
+        RaspiPin.GPIO_01, // LCD data bit 2
+        RaspiPin.GPIO_02, // LCD data bit 3
+        RaspiPin.GPIO_03); // LCD data bit 4
+
+    // provision gpio pins as input pins with its internal pull up resistor enabled
+    final GpioPinDigitalInput myButtons[] = {
+        gpio.provisionDigitalInputPin(RaspiPin.GPIO_13, "B1", PinPullResistance.PULL_UP),
+        gpio.provisionDigitalInputPin(RaspiPin.GPIO_07, "B2", PinPullResistance.PULL_UP),
+        gpio.provisionDigitalInputPin(RaspiPin.GPIO_04, "B3", PinPullResistance.PULL_UP),
+        gpio.provisionDigitalInputPin(RaspiPin.GPIO_12, "B4", PinPullResistance.PULL_UP) };
+
+    // create and register gpio pin listener
+    gpio.addListener(new GpioPinListenerDigital() {
+      @Override
+      public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+        if (event.getState() == PinState.LOW) {
+          lcd.writeln(LCD_ROW_2, event.getPin().getName() + " PRESSED", LCDTextAlignment.ALIGN_CENTER);
+        } else {
+          lcd.writeln(LCD_ROW_2, event.getPin().getName() + " RELEASED", LCDTextAlignment.ALIGN_CENTER);
+        }
+      }
+    }, myButtons);
 
     // clear LCD
-    out.clear();
+    lcd.clear();
     Thread.sleep(1000);
+
     // write line 1 to LCD
-    out.write(LCD_ROW_1, "- RPi LCD Java -");
+    lcd.write(LCD_ROW_1, "The Pi4J Project");
 
-    log.info("RPi LCD Java");
+    // write line 2 to LCD
+    lcd.write(LCD_ROW_2, "----------------");
 
-    return out;
+    // line data replacement
+    for (int index = 0; index < 5; index++) {
+      lcd.write(LCD_ROW_2, "----------------");
+      Thread.sleep(500);
+      lcd.write(LCD_ROW_2, "****************");
+      Thread.sleep(500);
+    }
+    lcd.write(LCD_ROW_2, "----------------");
+
+    // single character data replacement
+    for (int index = 0; index < lcd.getColumnCount(); index++) {
+      lcd.write(LCD_ROW_2, index, ">");
+      if (index > 0)
+        lcd.write(LCD_ROW_2, index - 1, "-");
+      Thread.sleep(300);
+    }
+    for (int index = lcd.getColumnCount() - 1; index >= 0; index--) {
+      lcd.write(LCD_ROW_2, index, "<");
+      if (index < lcd.getColumnCount() - 1)
+        lcd.write(LCD_ROW_2, index + 1, "-");
+      Thread.sleep(300);
+    }
+
+    // left alignment, full line data
+    lcd.write(LCD_ROW_2, "----------------");
+    Thread.sleep(500);
+    lcd.writeln(LCD_ROW_2, "<< LEFT");
+    Thread.sleep(1000);
+
+    // right alignment, full line data
+    lcd.write(LCD_ROW_2, "----------------");
+    Thread.sleep(500);
+    lcd.writeln(LCD_ROW_2, "RIGHT >>", LCDTextAlignment.ALIGN_RIGHT);
+    Thread.sleep(1000);
+
+    // center alignment, full line data
+    lcd.write(LCD_ROW_2, "----------------");
+    Thread.sleep(500);
+    lcd.writeln(LCD_ROW_2, "<< CENTER >>", LCDTextAlignment.ALIGN_CENTER);
+    Thread.sleep(1000);
+
+    // mixed alignments, partial line data
+    lcd.write(LCD_ROW_2, "----------------");
+    Thread.sleep(500);
+    lcd.write(LCD_ROW_2, "<L>", LCDTextAlignment.ALIGN_LEFT);
+    lcd.write(LCD_ROW_2, "<R>", LCDTextAlignment.ALIGN_RIGHT);
+    lcd.write(LCD_ROW_2, "CC", LCDTextAlignment.ALIGN_CENTER);
+    Thread.sleep(3000);
+
+    SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+
+    // update time
+    while (true) {
+      // write time to line 2 on LCD
+      if (gpio.isHigh(myButtons)) {
+        lcd.writeln(LCD_ROW_2, formatter.format(new Date()), LCDTextAlignment.ALIGN_CENTER);
+      }
+      Thread.sleep(1000);
+    }
   }
+  // stop all GPIO activity/threads by shutting down the GPIO controller
+  // (this method will forcefully shutdown all GPIO monitoring threads and
+  // scheduled tasks)
+  // gpio.shutdown(); <--- implement this method call if you wish to terminate the
+  // Pi4J GPIO controller}
 
   @PreDestroy
-  void preDestroy() throws Exception {
-
-    lcd().clear(LCD_ROW_1);
-    lcd().clear(LCD_ROW_2);
+  void preDestroy() {
+//
+//    lcd().clear(LCD_ROW_1);
+//    lcd().clear(LCD_ROW_2);
 
     log.info("LCD rows cleared");
   }
